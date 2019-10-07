ddc2bef9d7e2d3f244bbd46f88bad206a7954117
julienb86 <julien.bouafia@gmail.com>
2019-10-07 15:20:32 +0200

    added dropdowns

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,13 @@
 import Tooltip from "./ui/styles/tooltip";
+import Dropdown from "./ui/styles/dropdown";
 
 const tooltip = new Tooltip(document.querySelector(".tooltip"));
 
+const dropdowns = document.querySelectorAll(".dropdown");
+
 tooltip.init();
+
+dropdowns.forEach(dropdown => {
+  const instance = new Dropdown(dropdown);
+  instance.init();
+});
