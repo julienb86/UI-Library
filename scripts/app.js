88d367e193c56b6cfb76bc7e56f1ec08217b0fe5
julienb86 <julien.bouafia@gmail.com>
2019-10-08 16:42:44 +0200

    added snackbars

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,15 @@
 import Tooltip from "./ui/styles/tooltip";
 import Dropdown from "./ui/styles/dropdown";
 import Tables from "./ui/styles/tabs";
-
+import Snackbars from "./ui/styles/snackbars";
 const tooltip = new Tooltip(document.querySelector(".tooltip"));
 
 const dropdowns = document.querySelectorAll(".dropdown");
 
 const tables = new Tables(document.querySelector(".tabs"));
+
+const snackbars = new Snackbars();
+
 tooltip.init();
 
 dropdowns.forEach(dropdown => {
@@ -15,3 +18,11 @@ dropdowns.forEach(dropdown => {
 });
 
 tables.init();
+
+snackbars.init();
+
+const btn = document.querySelector(".btn");
+
+btn.addEventListener("click", () => {
+  snackbars.show("Tu m'as cliqué");
+});
