c856b5d16c5cd732b6d8a37044f916c17e4d60c2
julienb86 <julien.bouafia@gmail.com>
2019-10-09 19:12:42 +0200

    added close button functionality for the modal component

--- scripts/app.js
+++ scripts/app.js
@@ -4,25 +4,24 @@ import Tables from "./ui/styles/tabs";
 import Snackbars from "./ui/styles/snackbars";
 import Modals from "./ui/styles/modals";
 
+/* tooltip */
 const tooltip = new Tooltip(document.querySelector(".tooltip"));
-
-const dropdowns = document.querySelectorAll(".dropdown");
-
-const tables = new Tables(document.querySelector(".tabs"));
-
-const snackbars = new Snackbars();
-
-const modals = new Modals();
-
 tooltip.init();
+
+/* dropdown */
+const dropdowns = document.querySelectorAll(".dropdown");
 
 dropdowns.forEach(dropdown => {
   const instance = new Dropdown(dropdown);
   instance.init();
 });
 
+/* tabs */
+const tables = new Tables(document.querySelector(".tabs"));
 tables.init();
 
+/* snackbars */
+const snackbars = new Snackbars();
 snackbars.init();
 
 const btn = document.querySelector(".btn");
@@ -31,6 +30,8 @@ btn.addEventListener("click", () => {
   snackbars.show("Tu m'as cliqué");
 });
 
+/* modals */
+const modals = new Modals();
 modals.init();
 
 let btnModal = document.querySelector(".btn_active_modal");
@@ -39,6 +40,9 @@ btnModal.addEventListener("click", () => {
     "title",
     "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quis eaque odio minus vitae tenetur corporis esse tempore saepe hic laudantium fugiat est, labore magnam, eligendi asperiores ipsum tempora porro praesentium.",
     "Envoyer",
-    "Annuler"
+    "Annuler",
+    "x"
   );
 });
+
+modals.close();
--- scripts/ui/styles/modals.js
+++ scripts/ui/styles/modals.js
@@ -1,45 +1,56 @@
 import "./modal.css";
 
 class Modals {
-  constructor() {
+  constructor() {}
+  init() {
     this.modal = document.createElement("div");
     this.contentContainer = document.createElement("div");
+    //add a title, description and buttons
+    this.titleContainer = document.createElement("div");
     this.title = document.createElement("h3");
     this.content = document.createElement("p");
     this.confirmButton = document.createElement("button");
     this.cancelButton = document.createElement("button");
-    this.closeButton = document.createElement("button");
-  }
-  init() {
+    this.closeButton = document.createElement("span");
     //add a class to the main container
     this.modal.classList.add("modal");
     this.modal.style.display = "none";
 
-    //add a title, description and buttons
     this.title.classList.add("modal_title");
     this.content.classList.add("modal_content");
     this.contentContainer.classList.add("modal_container");
+    this.titleContainer.classList.add("modal_title_container");
     this.confirmButton.classList.add("btn");
     this.cancelButton.classList.add("btn");
     this.closeButton.classList.add("close");
-
+    this.title.appendChild(this.closeButton);
     /* append elements */
     document.querySelector("body").appendChild(this.modal);
     this.modal.appendChild(this.contentContainer);
-    this.contentContainer.appendChild(this.title);
+    this.contentContainer.appendChild(this.titleContainer);
+    this.titleContainer.appendChild(this.title);
+    this.titleContainer.appendChild(this.closeButton);
+
     this.contentContainer.appendChild(this.content);
     this.contentContainer.appendChild(this.confirmButton);
     this.contentContainer.appendChild(this.cancelButton);
-    this.title.appendChild(this.closeButton);
   }
-  show(title, content, confirmButton, cancelButton) {
+  show(title, content, confirmButton, cancelButton, spanButton) {
     //add active to class to show the modal
     this.modal.style.display = "block";
     this.title.textContent = title;
     this.content.textContent = content;
     this.confirmButton.textContent = confirmButton;
     this.cancelButton.textContent = cancelButton;
-    this.closeButton.textContent = "x";
+    this.closeButton.textContent = spanButton;
+  }
+  close() {
+    this.closeButton.addEventListener("click", () => {
+      this.modal.style.display = "none";
+    });
+    this.cancelButton.addEventListener("click", () => {
+      this.modal.style.display = "none";
+    });
   }
 }
 
